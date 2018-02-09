6c3dc9c370329bd9f82d9026d72bcca9fd30b49b
shavenking <shavenking@gmail.com>
2018-02-09 10:30:32 +0800

    Rework ViewController

--- swift-line-chart-demo/ViewController.swift
+++ swift-line-chart-demo/ViewController.swift
@@ -1,39 +1,46 @@
 import UIKit
 import QuartzCore
 
-class ViewController: UIViewController {
-  let lineChart = LineChart()
+// make(lineChart, fillUp: view)
+// fill(view, with: lineChart)
+// size(lineChart, sameWith: view)
+// constraint(lineChart, equalTo: view)
+fileprivate func constraint(_ child: UIView, equalTo parent: UIView) {
+  parent.addSubview(child)
+  child.translatesAutoresizingMaskIntoConstraints = false
+  child.widthAnchor.constraint(equalTo: parent.widthAnchor).isActive = true
+  child.heightAnchor.constraint(equalTo: parent.heightAnchor).isActive = true
+  child.centerYAnchor.constraint(equalTo: parent.centerYAnchor).isActive = true
+  child.centerXAnchor.constraint(equalTo: parent.centerXAnchor).isActive = true
+}
 
+fileprivate func make(numberOfSequence: Int, maxSize: UInt32) -> [[Float]] {
+  var integerSequences = [[Float]]()
+
+  for _ in stride(from: 0, to: numberOfSequence, by: 1) {
+    var integerSequence = [Float]()
+
+    for _ in stride(from: 0, to: arc4random_uniform(maxSize), by: 1) {
+      integerSequence.append(Float([1,2,3,4,5,6,7,8,9,10][Int(arc4random_uniform(10))]))
+    }
+
+    integerSequences.append(integerSequence)
+  }
+
+  return integerSequences
+}
+
+class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
-  }
 
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-  }
+    let lineChart = LineChart()
+    constraint(lineChart, equalTo: view)
 
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-    view.addSubview(lineChart)
-    lineChart.translatesAutoresizingMaskIntoConstraints = false
-    lineChart.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor).isActive = true
-    lineChart.heightAnchor.constraint(equalTo: view.layoutMarginsGuide.heightAnchor).isActive = true
-    lineChart.centerYAnchor.constraint(equalTo: view.layoutMarginsGuide.centerYAnchor).isActive = true
-    lineChart.centerXAnchor.constraint(equalTo: view.layoutMarginsGuide.centerXAnchor).isActive = true
-    for _ in stride(from: 0, to: 2, by: 1) {
-      var values = [Float]()
-      for _ in stride(from: 0, to: arc4random_uniform(1000), by: 1) {
-        values.append(Float([1,2,3,4,5,6,7,8,9,10][Int(arc4random_uniform(10))]))
-      }
-      lineChart.addLine(values: values)
-    }
-    lineChart.chunkSize = 3
+    make(numberOfSequence: 2, maxSize: 1000).forEach { lineChart.addLine(values: $0) }
+
+    lineChart.chunkSize = 7
     lineChart.reloadData()
-  }
-
-  override func viewDidLayoutSubviews() {
-    super.viewDidLayoutSubviews()
-    lineChart.collectionViewLayout.invalidateLayout()
   }
 }
 
